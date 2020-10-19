4208b13fad76eed0cb19f05964894e902a26d9f1
Leonardo Regalado <lregaladoz1900@alumno.ipn.mx>
2020-10-19 00:53:28 -0500

    Indice

--- FigurasMenu/FigurasMenu.java
+++ FigurasMenu/FigurasMenu.java
@@ -15,5 +15,17 @@ class FigurasMenu{
 
         System.out.println("Bienvenido al programa de calculo de areas y perimetros");
         System.out.println("Elija una de las siguientes opciones");
+        System.out.println("1-Calcular el area y perimetro de un cuadrado");
+        System.out.println("2-Calcular el area y perimetro de un triangulo");
+        System.out.println("3-Calcular el area y perimetro de un circulo");
+        System.out.println("4-Calcular el area y perimetro de un rectangulo");
+        System.out.println("5-Calcular el area y perimetro de un rombo");
+        System.out.println("6-Calcular el area y perimetro de un hexagono");
+        System.out.println("7-Salir");
+
+        opcion = entrada.nextInt();
+
+
+        switch
     }
 }

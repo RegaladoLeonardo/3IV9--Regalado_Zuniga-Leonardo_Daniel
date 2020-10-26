8341b578708bc6a6918445bc020b06d7d9e60866
Leonardo Regalado <lregaladoz1900@alumno.ipn.mx>
2020-10-26 13:38:50 -0600

    ExP1

---
+++ Evo/Evo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Evo{
+
+              public static void main(String[] args) {
+
+
+                            Scanner entrada = new Scanner(System.in);
+                            System.out.println( "Holas");
+              
+                            }   
+}

bcf3462e8fd7bcc94e2fc5381a31e5ec8eef46c9
Leonardo Regalado <lregaladoz1900@alumno.ipn.mx>
2020-12-09 22:27:23 -0600

    mmmmm

---
+++ Eventos_Mouse/src/eventos_mouse/Eventos_Mouse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventos_mouse;
+
+/**
+ *
+ * @author levar
+ */
+public class Eventos_Mouse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Window ventana= new Window();
+        ventana.set.Visible(true);
+    }
+    
+}
